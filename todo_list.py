f9b47b2b45972d5a258bc55983fab238b9acf4ca
sopheak_cs_student <cheasopheak917@gmail.com>
2025-10-15 21:32:32 +0700

    Night 2: To-do list with I/O file and input cleaning(strip)

--- todo_list.py
+++ todo_list.py
@@ -1,10 +1,8 @@
 todos = ["Do homework", "Buy Milk", "Go to school"]
-print(todos)
 for i in range(2):
-    todos.append(input("Enter tasks: "))
-    print(todos)
+    task = input("Enter tasks: ").strip()
+    todos.append(task)
 with open('todos.txt', 'w') as f:
     f.write("\n".join(todos))
 with open('todos.txt', 'r') as f:
     print(f.read())
-f.close()
