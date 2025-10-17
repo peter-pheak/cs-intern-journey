d5f866276f1938bfd59a6bca71bae6108d2c2297
sopheak_cs_student <cheasopheak917@gmail.com>
2025-10-17 16:49:24 +0700

    update try and execption

--- day_1.py
+++ day_1.py
@@ -2,7 +2,14 @@
 def add_numbers(a,b):
     return a + b
 for i in range(0,2):
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
+    num1 = num2 = 0
+    while True:
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+            break
+        except ValueError:
+            print("Please enter numeric values")
+            continue
     result = add_numbers(num1,num2)
     print(f"{num1} + {num2} = {result:0.2f}")
