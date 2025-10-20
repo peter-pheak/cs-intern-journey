d5f94893cd388ee140615c025bafca850dcd88d0
sopheak_cs_student <cheasopheak917@gmail.com>
2025-10-20 21:53:27 +0700

    Night 3: Add error handling in calculator

--- day_1.py
+++ day_1.py
@@ -2,7 +2,6 @@
 def add_numbers(a,b):
     return a + b
 for i in range(0,2):
-    num1 = num2 = 0
     while True:
         try:
             num1 = float(input("Enter first number: "))
@@ -10,6 +9,5 @@ for i in range(0,2):
             break
         except ValueError:
             print("Please enter numeric values")
-            continue
     result = add_numbers(num1,num2)
     print(f"{num1} + {num2} = {result:0.2f}")
