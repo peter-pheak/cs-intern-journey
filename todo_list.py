710ae29f3ac9240dcabf9330174f0e439b3a3760
sopheak_cs_student <cheasopheak917@gmail.com>
2025-10-20 23:49:11 +0700

    Night 5: Added interactive to-do menu with real-time updates

--- todo_list.py
+++ todo_list.py
@@ -1,16 +1,24 @@
 todos = ["Do homework", "Buy Milk", "Go to school"]
-for i in range(2):
-    task = input("Enter tasks: ").strip()
-    todos.append(task)
-print(" | ".join(todos))
-try:
-    index = int(input(f"Delete task by INDEX(0-{len(todos)}): "))
-    todos.pop(index)
-except ValueError:
-    print(">>Nothing changed(input error)<<")
-except IndexError:
-    print(">>Nothing changed(out of range error)<<")
-with open('todos.txt', 'w') as f:
-    f.write("\n".join(todos))
+while True:
+    try:
+        option = int(input("Enter 1: Add, 2: Delete, 3: View, 4: Exit: "))
+        if option == 1:
+            task = input("Enter tasks: ").strip()
+            todos.append(task)
+        elif option == 2:
+            index = int(input(f"Delete task by INDEX(0-{len(todos)-1}): "))
+            todos.pop(index)
+        elif option == 3:
+            print("\n".join(todos))
+        elif option == 4:
+            break
+        else:
+            print(">>Invalid input")
+        with open('todos.txt', 'w') as f:
+            f.write("\n".join(todos))
+    except ValueError:
+        print(">>Nothing changed(input error)<<")
+    except IndexError:
+        print(">>Nothing changed(out of range error)<<")
 with open('todos.txt', 'r') as f:
     print(f.read())
